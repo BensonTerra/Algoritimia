b6b224d299791ca60aa2119580355bc56a226fc3
BensonTerra <benson.terra@hotmail.com>
2023-02-15 19:57:47 +0000

    15/2/23

--- estudo/Estudo_Tkinter.py
+++ estudo/Estudo_Tkinter.py
@@ -29,8 +29,24 @@ def lerFicheiro():
     f = open(ficheiro, "r", encoding="utf-8")
     linhas = f.readlines()
     f.close()
+    print(linhas)
     return linhas
 lerFicheiro()
+
+#função para definir um inteiro indice para cada inscrição
+def lerFicheiroId():
+    f = open(ficheiro, "r", encoding="utf-8")
+    linhas = f.readlines()
+    num = len(linhas)
+    #print(num)
+    return num
+
+def carregarTreeView():
+    linhas = lerFicheiro()
+    for linha in linhas:
+        linha = linha.replace("\n","")
+        linha = linha.split(";")
+        print(linha)
 
 #Tela 0
 def TelaInicial():
@@ -41,6 +57,7 @@ def TelaInicial():
 
 #Tela 1
 def tela1():
+
     #-----------------------------------------------------------------------------------------------------------------------------#
     panned2Width = 0.75 * appWidth
     panned2Height = appHeight
@@ -66,27 +83,78 @@ def tela1():
     entry2 = Entry(labelFrame1, width = 10, font = ("Arial", 12), textvariable = txtUltimoNomeVar)
     entry2.place(x = 0.32 * panned2Width, y = 0.01 * panned1Height)
     #-----------------------------------------------------------------------------------------------------------------------------#
-    labelFrame2 = LabelFrame(labelFrame1, width = 0.4 * panned2Width, height = 0.05 * panned2Height, font = ("Arial", 12))
+    labelFrame2 = LabelFrame(labelFrame1, width = 0.4 * panned2Width, height = 0.15 * panned2Height, font = ("Arial", 12))
     labelFrame2.place(x = 0.01 * panned2Width, y = 0.05 * panned2Height)
-    labelFrame2Width = 0.1 * labelFrame2.winfo_width()
-    labelFrame2Height = 0.1 * labelFrame2.winfo_height()
+    labelFrame2Width = 0.4 * panned2Width
+    labelFrame2Height = 0.15 * panned2Height
     #---#
     selected = StringVar()
-    #selected.set("Docente")
-    rd1 = ttk.Radiobutton(labelFrame2Width, text = "Docente", value = "Docente", variable = selected)
-    rd2 = Radiobutton(labelFrame2Width, text = "Estudante")
-    rd3 = Radiobutton(labelFrame2Width, text = "Externo")
-    rd1.place(x = 0.1 * labelFrame2Width, y = 0.1 * labelFrame2Height)
-
+    selected.set("Docente")
+    rd1 = Radiobutton(labelFrame2, text = "Docente", value = "Docente", font = ("Arial", 12), variable = selected)
+    rd2 = Radiobutton(labelFrame2, text = "Estudante", value = "Estudante", font = ("Arial", 12), variable = selected)
+    rd3 = Radiobutton(labelFrame2, text = "Externo", value = "Externo", font = ("Arial", 12), variable = selected)
+    rd1.place(x = 0.05 * labelFrame2Width, y = 0.3 * labelFrame2Height)
+    rd2.place(x = 0.38 * labelFrame2Width, y = 0.3 * labelFrame2Height)
+    rd3.place(x = 0.70 * labelFrame2Width, y = 0.3 * labelFrame2Height)
+    #-----------------------------------------------------------------------------------------------------------------------------#
+    labelFrame3 = LabelFrame(labelFrame1, width = 0.4 * panned2Width, height = 0.2 * panned2Height, font = ("Arial", 12))
+    labelFrame3.place(x = 0.01 * panned2Width, y = 0.205 * panned2Height)
+    labelFrame3Width = 0.4 * panned2Width
+    labelFrame3Height = 0.2 * panned2Height
+    #---#
+    treePanel = ttk.Treeview(labelFrame3, columns=('Usuario','Perfil'),show='headings',height=6)
+    treePanel.heading('Usuario', text='Usuario')
+    treePanel.column('Usuario', width = int(0.67 * labelFrame3Width), anchor='c')
+    #---#
+    treePanel.heading('Perfil', text='Perfil')
+    treePanel.column('Perfil', width = int(0.25 * labelFrame3Width), anchor='c')
+    #---#
+    scrollBar1 = ttk.Scrollbar(labelFrame3, orient='vertical', command=treePanel.yview())
+    scrollBar1.place(x = 0.94 * labelFrame3Width,y = 0, height = 0.95 * labelFrame3Height)
+    #---#
+    treePanel.place(x = 0.01 * labelFrame3Width,y = 0.01 * labelFrame3Height)
+    #-----------------------------------------------------------------------------------------------------------------------------#
+    labelFrame4 = LabelFrame(labelFrame1, width = 0.4 * panned2Width, height = 0.3 * panned2Height, font = ("Arial", 12))
+    labelFrame4.place(x = 0.01 * panned2Width, y = 0.41 * panned2Height)
+    labelFrame4Width = 0.4 * panned2Width
+    labelFrame4Height = 0.25 * panned2Height
+    #---#
+    
+    btnLerFicheiro = Button(labelFrame4, text = "Ler Ficheiro", compound=LEFT, relief = "sunken", 
+                    width = 43, height = 3, font=("Arial", 12), command = lerFicheiro)
+    btnLerFicheiro.place(x = 0.01 * labelFrame4Width, y = 0.05 * labelFrame4Height)
+    #---#
+    btnLerAdicionar = Button(labelFrame4, text = "Adicionar", compound=LEFT, relief = "sunken", 
+                    width = 43, height = 3, font=("Arial", 12),command = lambda: adicionar(txtPrimeiroNomeVar.get(),txtUltimoNomeVar.get()))
+    btnLerAdicionar.place(x = 0.01 * labelFrame4Width, y = 0.42 * labelFrame4Height)
+    #---#
+    btnGuardar = Button(labelFrame4, text = "Guardar", compound=LEFT, relief = "sunken", 
+                    width = 43, height = 3, font=("Arial", 12))
+    btnGuardar.place(x = 0.01 * labelFrame4Width, y = 0.79 * labelFrame4Height)
+    
 #função 1
 def adicionar(PrimeiroNome,UltimoNome):
     os.system("cls")
-    print(PrimeiroNome)
-    print(UltimoNome)
-    f = open(ficheiro, "a", encoding="utf-8")
-    data = PrimeiroNome + ";" + UltimoNome +  ";" + "\n"
-    f.write(data)
-    f.close()
+    #print(PrimeiroNome)
+    #print(UltimoNome)
+    numsUsers = lerFicheiroId()
+    #print(numsUsers)
+    idUser = numsUsers + 1
+    idUser = str(idUser)
+    #print(idUser)
+    while PrimeiroNome != "" and UltimoNome != "":
+        print("Dentro do ciclo")
+        f = open(ficheiro, "a", encoding="utf-8")
+        data = idUser + ";" + PrimeiroNome + ";" + UltimoNome  + "\n"
+        f.write(data)
+        f.close()
+        break
+    if PrimeiroNome == "" or UltimoNome == "":
+        messagebox.showinfo(title = "Dados invalidos", message = "Primeiro Nome invalidou ou vazio")
+
+#função 2
+
+
 
 #Interface grafica
 window = Tk()
