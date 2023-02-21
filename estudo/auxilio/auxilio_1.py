5a2efe0372e294fb031829ac10a9ec70c24fd608
BensonTerra <benson.terra@hotmail.com>
2023-02-21 19:25:10 +0000

    a

--- estudo/auxilio/auxilio_1.py
+++ estudo/auxilio/auxilio_1.py
@@ -1,3 +1,82 @@
+"""----------------------------------------------------------------------------------------"""
+"""                                                                                        """
+"""                                       Codigos Genericos                                """
+"""                                                                                        """
+"""----------------------------------------------------------------------------------------"""
+
+#Função ler ficheiro, permite ler o ficheiro sem que seja necessario repeitir codigo no restante do sistema
+    def lerFicheiro():
+        f = open(ficheiro, "r", encoding="utf-8")
+        linhas = f.readlines()
+        f.close()
+        #print(linhas)
+        return linhas
+
+#Função para definir um identificador numerico, ou id, para cada dado a ser inserido
+#ex : id + ";" + "dados do exercicio / resultado: 1;dados..."
+
+    def lerFicheiroId():
+        f = open(ficheiro, "r", encoding="utf-8")
+        linhas = f.readlines()
+        num = len(linhas)
+        #print(num)
+        return num
+
+
+"""----------------------------------------------------------------------------------------"""
+"""                                                                                        """
+"""                                       Treeview Codigos                                 """
+"""                                                                                        """
+"""----------------------------------------------------------------------------------------"""
+
+#Carregar treeView com dados de um file.txt
+    def carregarAtualizarTreeView(treeView):
+        linhas = lerFicheiro()
+        treeView.delete(*treeView.get_children())
+        for linha in linhas:
+            linha = linha.replace("\n","")
+            linha = linha.split(";")
+            linha[1] = linha[1] + " " + linha[2]
+            treeView.insert("", END, values = (linha[1], linha[3]))
+
+#Função para deletar uma linha e reescrever o file.txt 
+    def indexCapture_TREEVIEW(treeView):
+        idSelecionado = treeView.focus()
+
+        item_id = treeView.index(idSelecionado)
+        item_id = item_id + 1
+        print(item_id)
+        return item_id
+
+#Função para remover elementos da treeView e atualizar a mesma
+    def removerUser(treeView):
+        os.system("cls")
+        print("remover")
+        linhas = lerFicheiro()
+        print(linhas)
+        index = indexCapture_TREEVIEW(treeView)
+        print(index)
+        #---#
+        idUser = 1
+        ptr = 1
+        acessos = open(ficheiro, "w", encoding="utf8")
+        for linha in linhas:
+            linha = linha.replace("\n","")
+            linha = linha.split(";")
+            print(ptr)
+            if ptr != index:
+                print("funciona")
+                data = """ * * * """
+                acessos.write(data)
+                idUser+=1
+            ptr+=1
+        acessos.close()
+        treeView.delete(*treeView.get_children())
+        carregarAtualizarTreeView(treeView)
+
+
+
+
 #Mostrar dados do elementos selecionado de uma treeView via event
     def item_selected_TREEVIEW(event):
         for selected_item in tree.selection():
