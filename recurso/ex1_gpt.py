afb85b57fe98abc70917dc098ce6aeed590ad3da
BensonTerra <benson.terra@hotmail.com>
2023-08-12 15:47:16 +0100

    ex1.py melhorado *nova estrutura de codigo

--- recurso/ex1_gpt.py
+++ recurso/ex1_gpt.py
@@ -1,33 +1,21 @@
-"""
-Gerar uma lista com 10 numeros aleatorios não repetido e o usuario de ve advinhar os mesmos
-"""
-
-import os #biblioteca os
-import random #biblioteca random
+import os
+import random
 
 listaResposta = []
-listaUsuario = ["*", "*", "*", "*", "*"]
-nArrayMax = 5
+listaUsuario = [50, 50, 50, 50, 50]
 
-#criaçao da lista de 10 numeros a ser aadvinhados
-def primeroCiclo():
-    global listaResposta
+def gerar_numeros_aleatorios():
     numeros_possiveis = list(range(1, 21))
-    listaResposta = random.sample(numeros_possiveis, nArrayMax)
-    print(listaResposta)
-primeroCiclo()
-
-#sistema do jogo
+    return random.sample(numeros_possiveis, 5)
+listaResposta = gerar_numeros_aleatorios()
 def jogo():
     i = 0
     global listaResposta
-    global nArrayMax
-    print(listaResposta)
     nArray = 0
     max_tentativas = 10
 
     while nArray < 5 and i < max_tentativas:
-        limparTela()
+        limpar_tela()
         print(listaResposta)
         print(listaUsuario)
         print(i)
@@ -43,7 +31,7 @@ def jogo():
         if num in listaResposta and num not in listaUsuario:
             index = listaResposta.index(num)
             listaUsuario[index] = num
-            print("Você acertou! O número %i esta na posição %i"%(num,index+1))
+            print(f"Você acertou! O número {num} está na lista na posição {index + 1}")
             nArray += 1
         elif num in listaUsuario:
             print("Você já digitou esse número")
@@ -54,42 +42,32 @@ def jogo():
         
         input()
     
-    limparTela()
-    if nArray == nArrayMax:
+    limpar_tela()
+    if nArray == 5:
         print("\n\t\t\tPARABÉNS!!!\n")
         print("\t\t\tVocê venceu")
     else:
         print("\n\t\t\tGAME OVER!\n")
         print("\t\t\tVocê não conseguiu adivinhar todos os números a tempo")
     input()
-    
-def limparTela():
+
+def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def reset():
-    global listaUsuario
-    listaUsuario = ["*", "*", "*", "*", "*"]
-
-#menu do jogo    
 def menu():
-    op=" "
-    os.system("cls")#Limpar tela
-    while op !="0":
-        os.system("cls")
+    op = " "
+    limpar_tela()
+    while op != "0":
+        limpar_tela()
         print("\n\t\t\tMENU\n")
-        print("\t\t1 --- jogar")
+        print("\t\t1 --- Jogar")
         print("\t\t0 --- Sair\n")
 
         op = input("\t\t    Opção: ")
         if op == '1':
-            print("teste 1")
-            reset()
             jogo()
 
-#Codigo principal incio
-autoStart="y"
-#-----------------------#
-while autoStart.upper() == "Y":
+autoStart = "S"
+while autoStart.upper() == "S":
     menu()
-    autoStart = input("Repetir(Y/N) ?: ")
-#Codigo principal fim
+    autoStart = input("Repetir (S/N) ?: ")
